ad45838dbc9a66f664f31b8e592e51ad8780e3c5
alekdoktor <46782206+alekdoktor@users.noreply.github.com>
2019-02-24 19:34:23 +0000

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,16 +1,17 @@
 from ROOT import *
 
 
-gInterpreter.LoadFile("./GaussSeidel_plot.cpp")
+gInterpreter.LoadFile("GaussSeidel_plot.cpp")
 
 
 
-n = 2;
+n = 5;
 
-length = 51;
+length = 11;
+
+binsWidth = 5./(length - 1)
 
 stencil = Stencil(n, length, length, 10, 10);
-
 
 
 matrix = MMatrix(stencil.get_values());
@@ -20,28 +21,23 @@ matrix = MMatrix(stencil.get_values());
 timeLoop = timeloop(stencil, matrix);
 
 
-
 c = TCanvas()
 
+gStyle.SetOptStat(0);
 
-
-
-
-
-h = TH2D("pot", "potential;x;y;#phi", length, -0.1, 10.1, length, -0.1, 10.1)
-
+h = TH2D("pot", "potential;x;y;#phi", length, -binsWidth, 10 + binsWidth, length, -binsWidth, 10 + binsWidth)
 
 
 for x in range(0, length):
-
+    
     for y in range(0, length):
-
+        
         xx = 10.*float(x) / float(length - 1)
-
+        
         yy = 10.*float(y) / float(length - 1)
-
+        
         binnr = h.FindBin(xx, yy)
-
+        
         h.SetBinContent(binnr, timeLoop[x][y])
 
 
